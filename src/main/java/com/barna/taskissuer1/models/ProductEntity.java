d43b2104e4c657fc4beec0949a6340ca8c496e51
Barna <barna90@gmail.com>
2018-05-21 21:42:45 +0200

    persistence

---
+++ src/main/java/com/barna/taskissuer1/models/ProductEntity.java
@@ -0,0 +1,63 @@
+package com.barna.taskissuer1.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product", schema = "sandbox")
+public class ProductEntity {
+    private int id;
+    private String name;
+    private Integer price;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "price")
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProductEntity that = (ProductEntity) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (price != null ? !price.equals(that.price) : that.price != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        return result;
+    }
+}
